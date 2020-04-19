709370a33ccf46f0c574b61313514a284013e489
Carl Erez <cerez@ucsc.edu>
2020-04-19 16:48:40 -0700

    Point breakdown added to Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,17 @@
+/*Name: Carl Erez
+    Altice
+    CMPM 120
+    4-19-20
+
+    Point Breakdown:
+        50: Simultaneous Local Multiplayer
+        25: New artwork for all assets (rocket-> orca, starship-> moose, explosion-> orca eating moose)
+        15: Create new title screen (recolored and organized text, and that logo was quite annoying to make read properly)
+                (Perhaps only worth 10, given that I generally kept the text box formatting)
+        10: Create new scrolling tile sprite for the background (stars-> ocean with small chunks of ice)
+        10: Allow the player to control the rocket after it is fired
+*/
+
 let config = {
     type: Phaser.WEBGL,
     width: 640,
